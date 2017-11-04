82f18b24029250b08dcd044bd4483d29a5b8ee5f
osumageed <mageed@oregonstate.edu>
2017-11-03 17:02:06 -0700

    Now ready to run test.  stdExp wasn't properly adjusted last time

--- src/stdExp.py
+++ src/stdExp.py
@@ -1,7 +1,7 @@
 '''
     Run experiment.
 '''
-
+#%%
 import os
 import os.path as op
 import sys
@@ -25,17 +25,19 @@ from main_help import *
 import result_help as rh
 import timing_help as th
 
+ext = ".json"
+
 eq = ["euler", "heat"];
 
 #makr = "nvcc solmain.cu jsoncpp.cpp -o ./bin/euler -gencode arch=compute_35,code=sm_35 -O3 -restrict -std=c++11 -I/usr/include/mpi -lmpi -Xcompiler -fopenmp -lm -w --ptxas-options=-v"
 #makr = shlex.split(makr)
 
+#%%
 nproc = 8
 mpiarg = "" #"--bind-to socket
 tstrng = os.listdir(testpath)
-schemes = ["C ", "S "]
+schemes = ["C", "S"]
 schD = {schemes[0]: "Classic", schemes[1]: "Swept"}
-
 #if op.isfile(prog):
 #
 #    sgrp = os.stat(prog)
@@ -60,36 +62,35 @@ schD = {schemes[0]: "Classic", schemes[1]: "Swept"}
 #    proc = sp.Popen(makr)
 #    sp.Popen.wait(proc)
 
-
+#%%
 #Say iterate over gpuA at one size and tpb
 gpus = [k/1.5 for k in range(1, 15)] #GPU Affinity
-nX = [2**k for k in range(11,21,2)] #Num spatial pts (Grid Size)
+nX = [2**k for k in range(12,21)] #Num spatial pts (Grid Size)
 tpb = [2**k for k in range(6,10)]
+ac = 0
 
+#%%
 for p in eq:
     prog = op.join(binpath, p)
     prog += " "
     eqs = [k for k in tstrng if p.upper() in k.upper()]
     eqspec = op.join(testpath, eqs[0])
     eqspec += " "
-    for s in schemes[1:]:
+    for sc in schemes:
+        timeTitle = "t"+p.title()+sc+ext
+        sc += " "
+        print(timeTitle)
         for t in tpb:
             for n in nX:
                 xl = int(n/10000) + 1
                 for g in gpus:
-                    exargs =  " freq 200 dt 0.0001 gpuA {:.4f} nX {:d} tpb {:d} lx {:d}".format(g, n, t, xl)
-                    runMPICUDA(prog, nproc, s, eqspec, mpiopt=mpiarg, eqopt=exargs, outdir=rawresultpath)
+                    exargs =  " freq 200 gpuA {:.4f} nX {:d} tpb {:d} lx {:d}".format(g, n, t, xl)
+                    runMPICUDA(prog, nproc, sc, eqspec, mpiopt=mpiarg, eqopt=exargs, outdir=rawresultpath)
 
-        rd = os.listdir(rawresultpath)
-        rd = [k for k in rd if k.startswith('t')] # t for timing
-        
-        for f in rd:
-            if s in f:
-                tfile = op.join(rawresultpath, f)
-                break
-        
+        tfile = op.join(rawresultpath, timeTitle)
+
         res = th.Perform(tfile)
-        eqn = eq + " " + schD[s]
+        eqn = eq + " " + schD[sc]
         res.plotframe(resultpath, eqn)
     
     

9cdfcc22b7baf5a8d8285af67d7d048427281de5
osumageed <mageed@oregonstate.edu>
2018-01-02 14:25:40 -0800

    Verging on reasonable output.  There were some flaws in the experiment.  It should be simple to correct them

--- runtools/compy.py
+++ runtools/compy.py
@@ -1,5 +1,9 @@
-import timing_help as th
+#!~/miniconda3/bin/python
 import sys
+
+import timing_help as th
+
+# run with ./compy.py equation scheme tpb gpuA nX
 
 myar = [float(k) for k in sys.argv[3:]]
 argstr = "tpb {:.2f} gpuA {:.2f} nX {:.2f}".format(*myar)
--- runtools/timing_helpStatmod.py
+++ runtools/timing_helpStatmod.py
@@ -1,6 +1,6 @@
 '''
     Classes and functions for plotting the actual results of the simulations.
-    Interpolating version
+
 '''
 
 # Dependencies: gitpython, palettable, cycler
@@ -19,9 +19,9 @@ import matplotlib.pyplot as plt
 import numpy as np
 import palettable.colorbrewer as pal
 import pandas as pd
-from scipy import interpolate
+import statsmodels.api as sm
+import statsmodels.formula.api as smf
 from cycler import cycler
-import re
 
 import result_help as rh
 from main_help import *
@@ -35,31 +35,24 @@ mpl.rcParams["grid.alpha"] = 0.5
 mpl.rcParams["axes.grid"] = True
 mpl.rcParams["savefig.dpi"] = 1000
 mpl.rcParams["savefig.bbox"] = "tight"
-
-# mpl.rcParams["figure.subplot.bottom"] = 0.08
-# mpl.rcParams["figure.subplot.right"] = 0.85
-# mpl.rcParams["figure.subplot.top"] = 0.9
-# mpl.rcParams["figure.subplot.wspace"] = 0.15
-# mpl.rcParams["figure.subplot.hspace"] = 0.25
-# mpl.rcParams["figure.subplot.left"] = 0.05
+mpl.rcParams["figure.subplot.bottom"] = 0.08
+mpl.rcParams["figure.subplot.right"] = 0.85
+mpl.rcParams["figure.subplot.top"] = 0.9
+mpl.rcParams["figure.subplot.wspace"] = 0.15
+mpl.rcParams["figure.subplot.hspace"] = 0.25
+mpl.rcParams["figure.subplot.left"] = 0.05
 
 ylbl = "Time per timestep (us)"
 xlbl = "Grid Size"
 ext = ".json"
 
-dftype=pd.core.frame.DataFrame #Compare types
+dftype=pd.core.frame.DataFrame
 
 schemes = {"C": "Classic", "S": "Swept"}
 
+cols = ["tpb", "gpuA", "nX", "time"]
+
 crs=["r", "b", "k", "g"]
-
-def formatSubplot(f, nsub):
-    if nsub == 1:
-        return f
-    elif nsub == 4:
-        f.subplots_adjust(top=0.9, bottom=0.08, right=0.85, left=0.05, wspace=0.15, hspace=0.25)
-    return f
-
 
 def find_all(name, path):
     result = []
@@ -80,15 +73,15 @@ def swapKeys(d):
 def parseCsv(fb):
     if isinstance(fb, str):
         jframe = pd.read_csv(fb)
-    elif isinstance(fb, dftype):
+    elif isinstance(fb, pd.core.frame.DataFrame):
         jframe = fb
 
     jframe.columns = cols
     jframe = jframe[(jframe.nX !=0)]
 
-    return jframe
+    return jframe        
 
-# Takes list of dfs? title of each df, longterm hd5, option to overwrite
+# Takes list of dfs? title of each df, longterm hd5, option to overwrite 
 # incase you write wrong.  Use carefully!
 def longTerm(dfs, titles, fhdf, overwrite=False):
     today = str(datetime.date(datetime.today()))
@@ -134,98 +127,157 @@ class Perform(object):
         self.cols = list(df.columns.values)
         self.xo = self.cols[:3]
         self.uniques, self.minmaxes = [], []
-
+            
         for k in self.xo:
             self.uniques.append(list(np.unique(self.oFrame[k])))
-            self.minmaxes.append( (min(self.uniques[-1]), max(self.uniques[-1])) )
-
+            self.minmaxes.append( (min(self.uniques[-1]), max(self.uniques[-1])) ) 
+            
         ap = []
         for k, g in self.oFrame.groupby('tpb'):
             for kk, gg in g.groupby('gpuA'):
                 ap.append(gg['nX'].max())
-
+                
         maxx = min(ap)
-
+        
         ap = []
         for k, g in self.oFrame.groupby('tpb'):
             for kk, gg in g.groupby('gpuA'):
                 ap.append(gg['nX'].min())
-
+                
         minx = max(ap)
         self.minmaxes[-1] = (minx, maxx)
-
-    def nxRange(self, buffer=10, nums=400):
-        nxs = np.logspace(*np.log2(self.minmaxes[-1]), base=2, num=nums)
-        return nxs[buffer:-buffer]
-
-    def fullTest(self, **kwargs):
-        nxs = nxRange(**kwargs)
-        # tpbs = np.arange(self.minmaxes[0][0], self.minmaxes[0][1]+1, 32)
-        gpuas = np.arange(self.minmaxes[1][0], self.minmaxes[1][1]+.01, .5)
-        iters = itertools.product(gpuas, nxs)
-        df = pd.DataFrame(list(iters), columns=self.xo[1:3])
-        # grd = np.meshgrid(gpuas, nxs)
+        
+    def fullTest(self):
+        nxs = np.logspace(*np.log2(self.minmaxes[-1]), base=2, num=1000)
+        nxs = nxs[100:900]
+        iters = itertools.product(self.uniques[0], self.uniques[1], nxs)
+        df = pd.DataFrame(list(iters), columns=self.xo)
         return df
-
+            
     def saveplot(self, cat, pt):
         #Category: i.e regression, Equation: i.e. EulerClassic , plot
         tplotpath = op.join(resultpath, cat)
         if not op.isdir(tplotpath):
             os.mkdir(tplotpath)
-
+            
         plotpath = op.join(tplotpath, self.title)
         if not op.isdir(plotpath):
             os.mkdir(plotpath)
-
+            
         plotname = op.join(plotpath, str(pt) + self.title + ".pdf")
         plt.savefig(plotname)
-
+        
     def __str__(self):
         ms = "%s \n %s \n Unique Exog: \n" % (self.title, self.oFrame.head())
-
+    
         for i, s in enumerate(self.uniques):
             ms = ms + self.cols[i] + ": \n " + str(s) + "\n"
         return ms
+        
 
+class perfModel(Perform):
+    def __init__(self, datadf, name, respvar):
+        #typ needs to tell it what kind of parameters to use
+        super().__init__(datadf, name)
+        self.respvar = respvar
+        
+        self.xof = self.xo[:]
+
+        for i, x in enumerate(self.xo):
+            for xa in self.xo[i:]:
+                if x == xa:
+                    self.xof.append("I(" + xa + "**2)")
+                else:
+                    self.xof.append(x + ":" + xa)
+        
+        self.formu = self.respvar + " ~ " + " + ".join(self.xof)
+            
+        print(self.formu)
+        self.mdl = smf.ols(formula=self.formu,  data=self.oFrame)
+        self.res = self.mdl.fit() 
+        self.pv = self.res.params
+        
+    def plotResid(self, saver=True):
+        for x in self.xo:
+            fig = sm.graphics.plot_regress_exog(self.res, x)
+            fig.suptitle(self.title + " " + x)
+            if saver:
+                self.saveplot("Residual", x)
+            else:
+                plt.show()
+     
     def plotRaw(self):
         for k, g in self.oFrame.groupby(self.xo[0]):
             if (k/32 %2):
                 f, a = plt.subplots(1, 1)
-
+                
                 for kk, gg in g.groupby(self.xo[1]):
-
+                    
                     a.semilogx(gg[self.xo[-1]], gg[self.respvar], label=str(kk))
-
+            
                 h, l = a.get_legend_handles_labels()
                 plt.legend(h,l)
                 plt.title(self.title + str(k))
-
+            
     def makedf(self, lists):
         if isinstance(lists[0], list):
             dct = {k:[] for k in self.xo}
             for a in lists:
                 for c, b in zip(self.xo, a):
                     dct[c].append(b)
-
+                    
         else:
             dct = dict(zip(self.xo, lists))
             dct = {0: dct}
-
+                
         return pd.DataFrame.from_dict(dct, orient='index')
 
     def predict(self, newVals):
         if not isinstance(newVals, dftype):
             newVals = self.makedf(newVals)
-
+        
         newFrame = newVals[self.xo]
-        return self.res.predict(newFrame)
-
+        return self.res.predict(newFrame)    
+    
     def model(self):
         xi = self.fullTest()
         fdf = self.res.predict(xi)
         xi[self.repvar] = fdf
         return xi.set_index(self.xo[0])
 
+    def bestG(self, n, th):
+        return -(self.pv["gpuA"] + self.pv["tpb:gpuA"]*th + self.pv["gpuA:nX"]*n)/(2.0*self.pv["I(gpuA ** 2)"]) 
+    
+    def buildGPU(self):
+        nxs = np.logspace(*np.log2(self.minmaxes[-1]), base=2, num=1000)
+        tpbs = np.arange(self.minmaxes[0][0], self.minmaxes[0][1]+1, 32)
+        tmpdf = pd.DataFrame(columns=self.xo[:-1])
+        dfg = tmpdf.copy()
+        tmpdf['tpb'] = tbps # self.uniques[0]
+        
+        for n in nxs:
+            tmpdf['nX'] = n
+            tmpdf['gpuA'] = self.bestG(tmpdf['nX'], tmpdf['tpb'])
+            tmpdf[self.respvar] = self.predict(tmpdf)
+            dfg = dfg.append(tmpdf.loc[tmpdf[self.respvar].idxmin()], ignore_index=True)
+    
+        return dfg
+
+    def plotFlop(self, df, f, a):
+        ncols=list(df.columns.values)
+        df = df.set_index(ncols[0])
+        nix=list(np.unique(df.index.values))
+        plt.hold(True)
+        for n, cr in zip(nix, crs):
+            dfn = df.xs(n)
+            print(dfn)
+            dfn.plot.scatter(*ncols[-2:], ax=a, label="{:1}".format(n), color=cr)
+            
+        a.set_title(self.title)    
+             
+    def pr(self):
+        print(self.res.summary(title=self.title))
+        
     def plotLine(self, plotpath=".", saver=True, shower=False):
         f, ax = plt.subplots(2, 2, figsize=(14,8))
         ax = ax.ravel()
@@ -247,9 +299,9 @@ class Perform(object):
             f.savefig(plotname, dpi=1000, bbox_inches="tight")
         if shower:
             plt.show()
-
+            
         return df
-
+        
     def plotContour(self, plotpath=".", saver=True, shower=False):
         f, ax = plt.subplots(2, 2, figsize=(14,8))
         ax = ax.ravel()
@@ -279,25 +331,51 @@ class Perform(object):
             f.savefig(plotname, dpi=1000, bbox_inches="tight")
         if shower:
             plt.show()
-
+            
         return df
+    
+    def polyAffinity(self, tpb, noX):
+        print(tpb, noX)
+        CC = self.pv["Intercept"] + self.pv["tpb"]*tpb + self.pv["nX"]* noX
+        CC += self.pv["tpb:nX"] * noX * tpb + self.pv["I(tpb ** 2)"] * tpb**2 + self.pv["I(nX ** 2)"] * noX**2
+        b = self.pv["gpuA"] + self.pv["tpb:gpuA"]*tpb
+        a = self.pv["I(gpuA ** 2)"]
+        return lambda g: a * g**2 + b * g + CC
+        
+    
+    def plotAffinity(self, ax, tpb):
+        nxs = np.round(np.arange(*np.log2(self.minmaxes[-1])))[-5:]
+        gr = np.linspace(*self.minmaxes[1], 100)
+        nxs = np.power(2, nxs)
+        for nx in nxs:
+            ply = self.polyAffinity(tpb, nx)
+            tm = ply(gr)
+            bflop = nx/tm
+            ax.plot(gr, bflop, label=str(int(nx)))
+            
+        h, l = ax.get_legend_handles_labels()
+        ax.set(xlabel="GPU Affinity", ylabel="Grid pts/us", title="tpb | {:d}".format(int(tpb)))
+        return h,l
+#        
+#        return gf, pG(gf)
 
 def predictNew(eq, alg, args, nprocs=8):
     oldF = mostRecentResults(resultpath)
     mkstr = eq.title() + schemes[alg].title()
     oldF.columns=cols
     oldF = oldF.xs(mkstr).reset_index(drop=True)
-    oldPerf = Perform(oldF, mkstr)
+    oldPerf = perfModel(oldF, mkstr)
     newMod = oldPerf.model()
-
+    
     argss = args.split()
     topics = ['tpb', 'gpuA', 'nX']
     confi = []
     for t in topics:
         ix = argss.index(t)
         confi.append(float(argss[ix+1]))
-
+        
     return oldPerf.predict(np.array(confi))
+
 
 # Change the source code?  Run it here to compare to the same
 # result in the old version
@@ -334,82 +412,75 @@ def compareRuns(eq, alg, args, nprocs=8): #)mdl=linear_model.LinearRegression())
     print(ratio)
     return ratio
 
+
 if __name__ == "__main__":
     recentdf = mostRecentResults(resultpath)
     cols = list(recentdf.columns.values)
-    print(cols)
-
+    
+#    rrg = linear_model.LinearRegression()
     timed = "time"
     timeLabel = "us per timestep"
     pointSpeed = "Speed"
     pointLabel = "MGridPts/s"
     recentdf[pointSpeed] = recentdf[cols[-2]]/recentdf[cols[-1]]
-    rVar = timed
+    rVar = pointSpeed
     rLabel = timeLabel
     perfs = []
     eqs = np.unique(recentdf.index.values)
-    pex = Perform(recentdf.xs(eqs[0]), eqs[0])
-    newx = pex.nxRange()
-    finalFrame = pd.DataFrame(index=newx, columns=eqs)
-    tFrame=pd.DataFrame(index=newx).rename_axis("nX")
-    sFrame=pd.DataFrame(index=newx).rename_axis("nX")
-    runtype=np.array([re.findall('[A-Z][^A-Z]*', k) for k in eqs])
-    runs = [np.unique(runtype[:,0]), np.unique(runtype[:,1])]
-    collFrame = collections.defaultdict(dict)
-    cFrame = collections.defaultdict(dict)
+    pex = perfModel(recentdf.xs(eqs[0]), eqs[0], rVar)
+    parms = pex.fullTest()    
+    nxs = parms['nX'].unique()
+    finalFrame = pd.DataFrame(index=nxs, columns=eqs)
+    sFrame=pd.DataFrame()
+    rawFrame = pd.DataFrame()
+
     for ty in eqs:
-        df = recentdf.xs(ty).reset_index(drop=True)
-        opt = re.findall('[A-Z][^A-Z]*', ty)
-        collectFrame = []
-        for k, g in df.groupby(cols[0]):
-            for kk, gg in g.groupby(cols[1]):
-                gg = gg.drop_duplicates('nX')
-                gin = interpolate.interp1d(gg[cols[2]], gg[rVar], kind='cubic')
-                bon = gin(newx)
-                # if np.sum(bon<0) > 0:
-                #     print(ty, k, kk)
-                #     print(gg)
-                #     plt.semilogx(newx, bon)
-                #     plt.show()
+        pmod = perfModel(recentdf.xs(ty), ty, rVar)
+        perfs.append(pmod)
+        parms[rVar] = pmod.predict(parms)
+        
+        for k, g in parms.groupby('tpb'):
+            sFrame[k] = g.pivot(index='nX', columns='gpuA', values=rVar).min(axis=1)
+            
+        finalFrame[ty] = sFrame.min(axis=1)
+            
+        print("------------")
+        pmod.pr()
+        pmodz = pmod.oFrame.set_index('nX')
+        rawFrame[ty] = pmodz[rVar]
+        
+        
+        pmod.plotRaw()
 
-                tFrame[kk] = bon
+    finalFrame.plot(loglog=True)
 
-            sFrame["tpb"] = k
-            sFrame["gpuA"] = tFrame.idxmin(axis=1)
-            sFrame["time"] = tFrame.min(axis=1)
-            appFrame = sFrame.reset_index()
-            collectFrame.append(appFrame)
+#    print("RSquared values for GLMs:")
+    pp = perfs[3]
+    stride = len(pp.uniques[0])/4
+    
+#    abc = pp.byFlop()
+#    coef = ["I(tpb ** 2)", "I(gpuA ** 2)", "tpb:gpuA"] #inflection test
+#    for p in perfs:
+#        dft = p.res.params
+#        a = 4*dft[coef[0]]*dft[coef[1]] - dft[coef[2]]**2
+#        f, a = plt.subplots(2,2)
+#        ax = a.ravel()
+#        tpbs = p.uniques[0]
+#        for i in range(4):
+#            idx = int(stride * i)
+#            ha, lb = p.plotAffinity(ax[i], tpbs[idx])
+#
+#        plt.legend(ha, lb, bbox_to_anchor=(1.05, 2), loc=2, title="Grid Size", borderaxespad=0.)
+#        f.suptitle(p.title, x=0.45, fontweight='bold')
+#        
+#        p.saveplot("Affinity", "wkstn")
+        
+    
+        
+        
+#    for p in perfs:
+#        p.plotResid()
+        
 
-        fullFrame = pd.concat(collectFrame)
-        print(ty, "----------------")
-        print(fullFrame.head())
-        fullFrame = fullFrame.pivot("nX", "tpb")
-        print(fullFrame.head())
-        print(fullFrame['time'].head())
+    
 
-        collFrame[opt[0]][opt[1]] = fullFrame
-        timef = fullFrame['time'].min(axis=1)
-        tpbf = fullFrame['time'].idxmin(axis=1)
-        cFrame[opt[0]][opt[1]] = timef
-
-        # f, a = plt.subplots(1, 2)
-        # aa = a.ravel()
-        # timef.plot(ax=aa[0], logx=True)
-        # aa[0].set_title("Best Time")
-        # aa[0].set_ylabel(ylbl)
-        # aa[0].set_xlabel(xlbl)
-        # tpbf.plot(ax=aa[1], logx=True)
-        # aa[1].set_title("Best Tpb")
-        # aa[1].set_ylabel("Threads per block")
-        # aa[1].set_xlabel(xlbl)
-        # plt.suptitle(ty)
-        # plt.show()
-
-    speedFrame = {}
-    for r in runs[0]:
-        speeder = cFrame[r]["Classic"]/cFrame[r]["Swept"]
-        speeder[speeder<0] = 0
-        speedFrame[r] = speeder
-        plt.semilogx(newx, speeder)
-        plt.title(r)
-        plt.show()

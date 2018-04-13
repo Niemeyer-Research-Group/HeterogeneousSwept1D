9f5d6c2f16ebcb733a6fea0626fd37ae04fe85bf
OSUmageed <mageed@oregonstate.edu>
2018-04-12 22:37:42 -0700

    Finished the geometry initialization but still need to assign processes to nodes- initialize values and make the pointer arrays.

--- src/twoD/decomposition/decomp.h
+++ src/twoD/decomposition/decomp.h
@@ -35,23 +35,22 @@ struct Node : public Location
 
 struct Globalism {
 // Topology
-    int nGpu, nPts;
-    int xg, xcpu;
-    int xStart;
-    int nWrite;
-    int hasGpu;
+    int nGpu, nPts, nWrite, hasGpu;
     double gpuA;
 
 // Geometry
 	int szState;
-    int tpb, tpbp, base;
-    int cBks, gBks;
+    int blockPts, blockSide, blockBase;
+    int nodeBlocks, nodeSidex, nodeSidey;
+    int gridNodes, gridSidex, gridSidey;
+    int blocksx, blocksy;
+    int gpux, gpuy;
+    int nx, ny;
     int ht, htm, htp;
-    int *dimNode, *dimBlocks;
 
 // Iterator
-    double tf, freq, dt, dx, lx;
-    bool bCond[2] = {false, false};
+    double tf, freq, dt, dx, dy, lx, ly;
+
 } cGlob;
 
 std::string fname = "GranularTime.csv";
@@ -85,9 +84,12 @@ void parseArgs(int argc, char *argv[])
 
 void initArgs()
 {
+    int *dimFinder;
+
 	cGlob.gpuA = inJ["gpuA"].asDouble();
 	int ranker = rank;
 	int sz = nprocs;
+
 	if (!cGlob.gpuA)
     {
         cGlob.hasGpu = 0;
@@ -99,6 +101,7 @@ void initArgs()
         MPI_Allreduce(&cGlob.hasGpu, &cGlob.nGpu, 1, MPI_INT, MPI_SUM, MPI_COMM_WORLD);
         sz--;
     }
+
     int *tFactor;
     tFactor = factor(cGlob.gpuA);
     if (cGlob.gpuA > 2)
@@ -109,25 +112,30 @@ void initArgs()
             tFactor = factor(cGlob.gpuA);
         }
     }
-
-
+    cGlob.gpux = tFactor[0];
+    cGlob.gpuy = tFactor[1];    
     // Wanted to standardize default values for these, but I realize that's not the point. Defaults are what the json is for.
     cGlob.lx = inJ["lx"].asDouble();
     cGlob.ly = inJ["ly"].asDouble();
     cGlob.szState = sizeof(states);
     int tpbReq = inJ["tpb"].asInt();
     cGlob.blockSide = std::sqrt(tpbReq);
-    cGlob.tpb = cGlob.blockSide * cGlob.blockSide;
+    cGlob.blockPts = cGlob.blockSide * cGlob.blockSide;
+    inJ["blockSide"] = cGlob.blockSide;
+    cGlob.blockBase = cGlob.blockSide+2;
+    inJ["blockBase"] = cGlob.blockBase;
 
     cGlob.dt = inJ["dt"].asDouble();
     cGlob.tf = inJ["tf"].asDouble();
     cGlob.freq = inJ["freq"].asDouble();
-    cGlob.nNodes = sz + (cGlob.nGpu * cGlob.gpuA);
-    cGlob.dimNode = factor(cGlob.nNodes);
+    if (!cGlob.freq) cGlob.freq = cGlob.tf*2.0;
+
+    cGlob.gridNodes = sz + (cGlob.nGpu * cGlob.gpuA);
+    dimFinder = factor(cGlob.gridNodes);
     
-    while (cGlob.dimNode[0] != 1)
+    while (dimFinder[0] != 1)
     {
-        if (!nGpu)
+        if (!cGlob.nGpu)
         {
             std::cout << "You provided a prime number of processes and no GPU capability. That's just silly and you absolutely cannot do it.  Retry!" << std::endl;
         }
@@ -138,80 +146,56 @@ void initArgs()
             cGlob.gpuA++;
             tFactor = factor(cGlob.gpuA);
         }
-        cGlob.nNodes = sz + (cGlob.nGpu * cGlob.gpuA);
-        cGlob.dimNode = factor(cGlob.nNodes);
+        cGlob.gridNodes = sz + (cGlob.nGpu * cGlob.gpuA);
+        dimFinder = factor(cGlob.gridNodes);
     }
 
+    cGlob.gridSidex = dimFinder[0];
+    cGlob.gridSidey = dimFinder[1];
+
+    // Something that makes this work if you give it nodeBlocks rather than nPts.
     cGlob.nPts = inJ["nPts"].asInt();
-    cGlob.dimBlocks = factor(cGlob.cBlocks);
+    cGlob.nodeBlocks = cGlob.nPts/(cGlob.blockPts * cGlob.gridNodes);
+    dimFinder = factor(cGlob.nodeBlocks);
+
+    while (cGlob.dimFinder[0] != 1)
+    {
+        cGlob.nodeBlocks++;
+        dimFinder = factor(cGlob.nodeBlocks);
+    }
+    cGlob.nPts = cGlob.blockPts * cGlob.nodeBlocks * cGlob.gridNodes;
+    cGlob.nodeSidex = dimFinder[1];
+    cGlob.nodeSidey = dimFinder[0];
+    cGlob.blocksx = cGlob.nodeSidex * cGlob.gridSidex;
+    cGlob.blocksy = cGlob.nodeSidey * cGlob.gridSidey;
+    cGlob.nx = cGlob.blocksx * cGlob.blockSide;
+    cGlob.ny = cGlob.blocksy * cGlob.blockSide;
 
     // -- Here ish
 
-
-    if (!cGlob.freq) cGlob.freq = cGlob.tf*2.0;
-
-    if (inJ["nX"].asInt() == 0)
-    {
-        if (inJ["cBks"].asInt() == 0)
-        {
-            cGlob.gBks = inJ["gBks"].asInt();
-            cGlob.cBks = cGlob.gBks/cGlob.gpuA;
-        }
-        else
-        {
-            cGlob.cBks = inJ["cBks"].asInt();
-            cGlob.gBks = cGlob.cBks*cGlob.gpuA;
-        }
-        if (cGlob.cBks<2) cGlob.cBks = 2; // Floor for cpu blocks per proc.
-        if (cGlob.cBks & 1) cGlob.cBks++;
-    }
-    else
-    {
-        cGlob.nX = inJ["nX"].asInt();
-        cGlob.cBks = round(cGlob.nX/(cGlob.tpb*(nprocs + cGlob.nGpu * cGlob.gpuA)));
-        if (cGlob.cBks & 1) cGlob.cBks++;
-        cGlob.gBks = cGlob.gpuA*cGlob.cBks;
-    }
-    // Need to reset this after figuring out partitions.
-
-    cGlob.nX = cGlob.tpb * (nprocs * cGlob.cBks + cGlob.nGpu * cGlob.gBks);
-
-    cGlob.base = cGlob.tpb+2;
-    cGlob.tpbp = cGlob.tpb+1;
-    cGlob.ht = cGlob.tpb/2;
+    cGlob.ht = cGlob.blockSide/2;
     cGlob.htm = cGlob.ht-1;
     cGlob.htp = cGlob.ht+1;
     // Derived quantities
-    cGlob.xcpu = cGlob.cBks * cGlob.tpb;
-    cGlob.xg = cGlob.gBks * cGlob.tpb;
 
-    // inJ["gpuAA"] = (double)cGlob.gBks/(double)cGlob.cBks; // Adjusted gpuA.
-    inJ["cBks"] = cGlob.cBks;
-    inJ["gBks"] = cGlob.gBks;
-    inJ["nX"] = cGlob.nX;
-    inJ["xGpu"] = cGlob.xg;
-    inJ["xCpu"] = cGlob.xcpu;
-
-    // Different schemes!
-    cGlob.dx = cGlob.lx/(double)cGlob.nX; // Spatial step
+    cGlob.dx = cGlob.lx/(double)cGlob.nx; // Spatial step
+    cGlob.dy = cGlob.ly/(double)cGlob.ny; // Spatial step
     cGlob.nWrite = cGlob.tf/cGlob.freq + 2;
-    inJ["dx"] = cGlob.dx; // To send back to equation folder.  It may need it, it may not.
+    inJ["dx"] = cGlob.dx; // To send back to equation folder. 
+    inJ["dy"] = cGlob.dy;
 
-    equationSpecificArgs(inJ);
+    HCONST.init(inJ);
+    cudaMemcpyToSymbol(DCONST, &HCONST, sizeof(HCONST));
 
-    // Swept Always Passes!
-
-    // If BCTYPE == "Dirichlet"
-    if (rank == 0) cGlob.bCond[0] = false;
-    if (rank == lastproc) cGlob.bCond[1] = false;
-    // If BCTYPE == "Periodic"
-        // Don't do anything.
-    if (!rank)  cout << rank <<  " - Initialized Arguments" << endl;
-
+    if (!rank)  std::cout << rank <<  " - Initialized Arguments -" << std::endl;
 }
 
-void solutionOutput(states *outState, double tstamp, int idx, int strt)
+// I THINK THIS SHOULD BE IN THE NODE STRUCT
+void solutionOutput(states *outState, double tstamp, const int idx, const int idxy)
 {
+    #ifdef NOS
+        return; // Prevents write out in performance experiments so they don't take all day.
+    #endif
     std::string tsts = std::to_string(tstamp);
     double xpt = indexer(cGlob.dx, idx, strt);
     std::string xpts = std::to_string(xpt);
@@ -220,66 +204,6 @@ void solutionOutput(states *outState, double tstamp, int idx, int strt)
         solution[outVars[k]][tsts][xpts] = printout(outState + idx, k);
     }
 }
-
-void writeOut(states **outState, double tstamp)
-{
-    #ifdef NOS
-        return; // Prevents write out in performance experiments so they don't take all day.
-    #endif
-    static const int ax[2] = {cGlob.xcpu/2, cGlob.xg};
-    static const int bx[3] = {cGlob.xStart, cGlob.xStart+ax[0], cGlob.xStart+ax[0]+ax[1]};
-    int k;
-
-    if (cGlob.hasGpu)
-    {
-        for (int i=0; i<3; i++)
-        {
-            for(k=1; k<=ax[i&1]; k++)
-            {
-                solutionOutput(outState[i], tstamp, k, bx[i]);
-            }
-        }
-    }
-    else
-    {
-        for(k=1; k<=cGlob.xcpu; k++)
-        {
-            solutionOutput(outState[0], tstamp, k, cGlob.xStart);
-        }
-    }
-}
-
-
-struct cudaTime
-{
-    std::vector<double> times;
-    cudaEvent_t start, stop;
-	float ti;
-    std::string typ = "GPU";
-
-    cudaTime() {
-        cudaEventCreate( &start );
-	    cudaEventCreate( &stop );
-    }
-    ~cudaTime()
-    {
-        cudaEventDestroy( start );
-	    cudaEventDestroy( stop );
-    }
-
-    void tinit(){ cudaEventRecord( start, 0); }
-
-    void tfinal(){ 
-        cudaEventRecord(stop, 0);
-	    cudaEventSynchronize(stop);
-	    cudaEventElapsedTime(&ti, start, stop);
-        times.push_back(ti); 
-        }
-
-    int avgt() { 
-        return std::accumulate(times.begin(), times.end(), 0)/ times.size();
-        }
-};
 
 struct mpiTime
 {

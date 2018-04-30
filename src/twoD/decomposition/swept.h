1a13809ecb24b340bae7ac82ed178a405542ea3a
OSUmageed <mageed@oregonstate.edu>
2018-04-29 21:14:10 -0700

    Bridges and all computation done, passing arrays complete.  Pass Procedure and main function are iffy.  Also I think I need a separate function, or some flag for SW bridges.  Perhaps.

--- src/twoD/decomposition/swept.h
+++ src/twoD/decomposition/swept.h
@@ -6,11 +6,79 @@
 
 struct sweptConst
 {
-    int sideSmem, rBase, hBridgeOffset, vBridgeOffset, splitOffset;
+    int sideSmem, rBase, hBridgeOffset, vBridgeOffset, sharedOffset, splitOffset;
 };
 
-sweptConst dSweptConst;
-__constant__ sweptConst hSweptConst;
+struct passIndex
+{
+    //Edges to pass;
+    const int side, base;
+    int nPass, sizePass;
+    int *pyramid, *bridge;
+    int **pyr[4];
+    int **brg[4];
+    
+    passIndex(int side, int base) : side(side), base(base), nPass(nPass)
+    {
+        nPass       = side/4 * (side + 2) + (side + 2);
+        sizePass    = nPass * sizeof(states);
+        pyramid     = new int[4 * nPass];
+        bridge      = new int[4 * nPass];
+        for (int k=0; k<4; k++)
+        {
+            pyr[k]  = (int *) (pyramid + nPass*k);
+            brg[k]  = (int *) (pyramid + nPass*k);  
+        }
+
+        this->initialize()
+    }
+    ~passIndex()
+    {
+        delete[] pyramid;
+        delete[] bridge;
+    }
+    void initialize()
+    {
+        int ca[4] = {0};
+        int flatidx;
+        // Pyramid Loop
+        for (int ky = 1; ky<=side; ky++)
+        {
+            for (int kx = 1; kx<=side; kx++)
+            {
+                flatidx = ky * base + kx;
+                if (x <= y){
+                    if (x+y <= side+1) pyr[1][ca[1]++] = flatidx;
+                    if (x+y >= side+1) pyr[2][ca[2]++] = flatidx;
+                }
+                if (x >= y){
+                    if (x+y <= side+1) pyr[0][ca[0]++] = flatidx;
+                    if (x+y >= side+1) pyr[3][ca[3]++] = flatidx;
+                }
+            }
+        }
+        // Bridge Loop
+        for (int i=0; i<3; i++) ca[i] = 0;
+        for (int ky = 0; ky<=side; ky++)
+        {
+            for (int kx = 0; kx<=side; kx++)
+            {
+                flatidx = ky * base + kx;
+                if (x <= y){
+                    if (x+y <= side) brg[1][ca[1]++] = flatidx;
+                    if (x+y >= side) brg[2][ca[2]++] = flatidx;
+                }
+                if (x >= y){
+                    if (x+y <= side) brg[0][ca[0]++] = flatidx;
+                    if (x+y >= side) brg[3][ca[3]++] = flatidx;
+                }
+            }
+        }
+    }
+};
+
+__constant__ sweptConst dSweptConst;
+sweptConst hSweptConst;
 
 __global__ void horizontalBridge(states **state, const int ts);
 __global__ void verticalBridge(states **state, const int ts);
@@ -18,60 +86,75 @@ __global__ void wholeOctahedron(states **state, const int ts);
 
 // Can it work for host too?
 __device__ int 
-upPyramid(states *state, const int ts, const int tol, const int base)
+upPyramid(states *state, int ts, const int tol, const int base)
 {
     int sid;
     int kx, ky, kt;
-    for (kt = 1; kt<tol; kt++)
+    for (kt = 1; kt<=tol; kt++)
     {
-        for (ky = threadIdx.y + kt; ky<base + kt; ky+=blockDim.y)
+        for (ky = threadIdx.y + kt; ky<(base - kt); ky+=blockDim.y)
         {
-            for(kx = threadIdx.x + kt; kx<base + kt; kx+=blockDim.x) 
-            {
-                sid =  ky * base + kx;
-                stepUpdate(state, sid, ts+kt);
+            for(kx = threadIdx.x + kt; kx<(base - kt); kx+=blockDim.x)
+            {       
+                sid =  ky * dSweptConst.rBase  + kx;
+                stepUpdate(state, sid, ts++);
             }
         }    
         __syncthreads();
     }
-    return ts + kt; // Int2 to return place?  Or isn't that already tol.
+    return ts; 
 }
 
 __device__ int 
-downPyramid(states *state, const int ts, const int tol, const int base)
+downPyramid(states *state, int ts, const int tol, const int base)
 {
     int sid;
     int kx, ky, kt;
-    for (kt = 1; kt<tol; kt++)
+    for (kt=tol; kt>0; kt--)
     {
-        for (ky = threadIdx.y + kt; ky>base - kt; ky+=blockDim.y)
+        for (ky = threadIdx.y + kt; ky<(base - kt); ky+=blockDim.y)
         {
-            for(kx = threadIdx.x + kt; ky>base - kt; kx+=blockDim.x) 
+            for(kx = threadIdx.x + kt; kx<(base - kt); kx+=blockDim.x) 
             {
-                sid =  ky * base + kx;
-                stepUpdate(state, sid, ts+kt);
+                sid =  ky * dSweptConst.rBase + kx;
+                stepUpdate(state, sid, ts++);
             }
         }    
         __syncthreads();
     }
-    return ts + k; // Int2 to return place?  Or isn't that already tol
+    return ts; 
 }
 
 __device__ void
-swapMem(states *toState, states *fromState, const int to_off, const int from_off)
+swapMem(states *toState, states *fromState, const bool to_global)
 {
-    int sid;
+    int tid, fid, tbase, fbase, toff, foff;
+    if (to_global)
+    {
+        fbase   = dSweptConst.sideSmem;
+        tbase   = dSweptConst.rBase;
+        toff    = dSweptConst.sharedOffset;
+        foff    = 0;
+    }
+    else
+    { 
+        fbase   = dSweptConst.rBase;
+        tbase   = dSweptConst.sideSmem;
+        toff    = 0;
+        foff    = dSweptConst.sharedOffset;
+    }
+
     for (ky = threadIdx.y; ky<=dSweptConst.sideSmem; ky+=blockDim.y)
     {
-        for(kx = threadIdx.x; ky<=dSweptConst.sideSmem; kx+=blockDim.x) 
+        for (kx = threadIdx.x; kx<=dSweptConst.sideSmem; kx+=blockDim.x) 
         {
-            sid =  ky * dSweptConst.sideSmem + kx;
-            toState[sid + to_off] = fromState[sid + from_off]
+            fid             = ky * fbase + kx + foff;
+            tid             = ky * tbase + kx + toff
+            toState[tid]    = fromState[fid];
         }
     }    
+    __syncthreads();
 }
-
-
 
 // Int not const because needs to be handled between calls.  Return with device function.
 // Type 0 for downPyramid, 1 for upPyramid, 2 for wholeOcto 
@@ -80,92 +163,153 @@ __global__ void
 wholeOctahedron(states **regions, int ts, const bool split, const int type)
 {
     //Launch 1D grid of 2d Blocks
-
     states *blkState = ((states *) regions[blockIdx.x]) + split * dSweptConst.splitOffset; 
     extern __shared__ states sharedState[];
-    int kx = threadIdx.x + 1; 
-    int ky = threadIdx.y + 1;
     
+    // Down pyr shared -> global.
     if (type-1)
     {   
-        swapMem(sharedState, blkState, 0, );
-        ts = downPyramid(sharedState, ts, , dSweptConst.sideSmem);
-        swapMem(blkState, sharedState, , 0);
-        ts = downPyramid(blkState, ts, , dSweptConst.rBase);
+        swapMem(sharedState, blkState, false);
+        ts = downPyramid(sharedState, ts, dSweptConst.sideSmem/2, dSweptConst.sideSmem);
+        swapMem(blkState, sharedState, true);
+        ts = downPyramid(blkState, ts, dSweptConst.sharedOffset, dSweptConst.rBase);
     }
     if (type)
     {
-        ts = upPyramid(blkState, ts, , dSweptConst.rBase);
-        swapMem(sharedState, blkState, 0, );
-        ts = upPyramid(sharedState, ts, , dSweptConst.sideSmem);
-        swapMem(blkState, sharedState, , 0);
+        ts = upPyramid(blkState, ts, dSweptConst.sharedOffset, dSweptConst.rBase);
+        swapMem(sharedState, blkState, false);
+        ts = upPyramid(sharedState, ts, dSweptConst.sideSmem/2, dSweptConst.sideSmem);
+        swapMem(blkState, sharedState, true);
     }
 }
 
-__global__ void horizontalBridge(states **regions, int ts)
+// It probably needs another parameter for the offset case
+__global__ void homeBridges(states **regions, int ts)
 {
     //Launch 1D grid of 2d Blocks
-
-    states *blkState = ((states *) regions[blockIdx.x]) + dSweptConst.hBridgeOffset; 
-    extern __shared__ states sharedState[];
-}
-__global__ void verticalBridge(states **regions, int ts)
-{
-    //Launch 1D grid of 2d Blocks
-
-    states *blkState = ((states *) regions[blockIdx.x]) +  dSweptConst.vBridgeOffset; 
-    extern __shared__ states sharedState[];
+    states *horizBridge = ((states *) regions[blockIdx.x]) +  dSweptConst.hBridgeOffset; 
+    states *vertBridge = ((states *) regions[blockIdx.x]) +  dSweptConst.vBridgeOffset; 
+    // extern __shared__ states sharedState[];
+    int sidh, sid2, dimStart;
+    int rStart = (DCONST.regionSide - 2)/2;
+    int kx, ky, kt;
+    for (kt=rStart; kt>0; kt--)
+    {
+        dimStart = 1 + (rStart-kt);
+        for (ky = threadIdx.y + dimStart; ky<(DCONST.regionBase - kt); ky+=blockDim.y)
+        {
+            for(kx = threadIdx.x + kt; kx<(DCONST.regionSide - kt); kx+=blockDim.x) 
+            {
+                sidh =  ky * dSweptConst.rBase + kx;
+                stepUpdate(horizBridge, sidh, ts++);
+                sidv =  kx * dSweptConst.rBase + ky;
+                stepUpdate(vertBridge, sidv, ts++);
+            }
+        }    
+        __syncthreads();
+    }
 }
 
 /*
     MARK : HOST SWEPT ROUTINES
 */
-
-
-void wholeOctahedronCPU(states *state, int tnow)
+// USE SPLIT FLAG AND OFFSET OR APPLY OFFSET IN MAIN?
+void wholeOctahedronCPU(states *state, int tnow, const int type)
 {
-    for (int k=cGlob.ht; k>0; k--)
-    {
-        for (int n=k; n<(cGlob.base-k); n++)
+    int h, x, y;
+    int sid;
+    int fromEnd;
+    if (type-1)
+    {   
+        for (h=cGlob.ht, h>0; h--)
         {
-            stepUpdate(state, n, tnow);
+            fromEnd=cGlob.base-h;
+            for (y=h; y<fromEnd; y++)
+            {
+                for (x=h; x<fromEnd; x++)
+                {
+                    sid = y*cGlob.fullStep + x;
+                    stepUpdate(state, sid, tnow++);
+                }
+            }
+            tnow++;
         }
-	tnow++;
     }
-
-    for (int k=2; k<cGlob.htp; k++)
+    if (type)
     {
-        for (int n=k; n<(cGlob.base-k); n++)
+        for (h=1, h<cGlob.ht; h++)
         {
-            stepUpdate(state, n, tnow);
+            fromEnd=cGlob.base-h;
+            for (y=h; y<fromEnd; y++)
+            {
+                for (x=h; x<fromEnd; x++)
+                {
+                    sid = y*cGlob.fullStep + x;
+                    stepUpdate(state, sid, tnow++);
+                }
+            }
+            tnow++;
         }
-    tnow++;
     }
 }
 
-void horizontalBridgeCPU()
+void homeBridgeCPU(states *region, int ts)
 {
-
+    //Launch 1D grid of 2d Blocks
+    states *horizBridge = ((states *) regions +  hSweptConst.hBridgeOffset); 
+    states *vertBridge = ((states *) regions +  hSweptConst.vBridgeOffset); 
+    // extern __shared__ states sharedState[];
+    int sidh, sid2, dimStart;
+    int rStart = (HCONST.regionSide - 2)/2;
+    int kx, ky, kt;
+    for (kt=rStart; kt>0; kt--)
+    {
+        dimStart = 1 + (rStart-kt);
+        for (ky = dimStart; ky<(HCONST.regionBase - kt); ky++)
+        {
+            for(kx = kt; kx<(HCONST.regionSide - kt); kx++) 
+            {
+                sidh =  ky * hSweptConst.rBase + kx;
+                stepUpdate(horizBridge, sidh, ts++);
+                sidv =  kx * hSweptConst.rBase + ky;
+                stepUpdate(vertBridge, sidv, ts++);
+            }
+        }    
+        __syncthreads();
+    }
 }
-
-void verticalBridgeCPU()
-{
-    
-}
-
-
 /*
     MARK : PASSING ROUTINES
 */
-cout
 
-<template T>
-int smemsize(T *func)
+// I THINK WE NEED ANOTHER BRIDGE ROUTINE BUT AS FAR AS I CAN TELL IT'S DONE TO HERE.
+__global__
+void passGPU(states *put, states *get, const int *idxmaps)
 {
-    struct cudaFuncAttributes attr;
-    memset(&attr, 0, sizeof(attr));
-    cudaFuncGetAttributes(&attr, func);
-    return attr.maxDynamicSharedSizeBytes/8;
+    const int putidx    = //(getidx+2) & 3;
+    const int gid       = 1 + threadIdx.x + blockDim.x * blockIdx.x; 
+    
+    if (gid>A.regionSide) return;
+}
+
+__global__
+void collectGPU()
+{
+
+}
+
+void collectCPU()
+{
+
+}
+
+int smemsize()
+{
+    int dev;
+    cudaGetDevice(&dev);
+    cudaDeviceProp deviceProp;
+    cudaGetDeviceProperties(&deviceProp, dev);
+    return deviceProp.sharedMemPerBlock;
 }
 
 void sweptWrapper(std::vector <Region *> &regionals)
@@ -175,44 +319,68 @@ void sweptWrapper(std::vector <Region *> &regionals)
     const int gpuRegions = cGlob.hasGpu * regionals.size();
     states **regionSelector;
     
-    int buffersize = "I've Figured this out";
+    int bigBuffer = (cGlob.regionSide * (cGlob.regionSide + 2)) / 4 + cGlob.regionBase;
     for (auto r: regionals) r->makeBuffers(buffersize, 1);
     dim3 tdim(cGlob.tpbx,cGlob.tpby);
     const int minLaunch = cGlob.regionSide/1024 + 1;
     int stepNow = regionals[0]->tStep;
 
+    size_t smem                 = smemsize();
+    int nSmem                   = smem/sizeof(states);
+    int sharedCoord             = (cGlob.regionSide + )
+    hSweptConst.rBase           = regionals[0].fullSide;
+    hSweptConst.sideSmem        = (std::sqrt(nSmem)/2) * 2;
+    smem                        = hSweptConst.sideSmem * hSweptConst.sideSmem * sizeof(states);
+    hSweptConst.splitOffset     = cGlob.ht * (hSweptConst.rBase + 1);
+    hSweptConst.vBridgeOffset   = hSweptConst.rBase * cGlob.htp + 1;
+    hSweptConst.hBridgeOffset   = hSweptConst.rBase + cGlob.htp;
+    int sharedCoord             = (cGlob.regionSide - hSweptConst.sideSmem)/2;
+    hSweptConst.sharedOffset    = sharedCoord * (hSweptConst.rBase + 1);
+
+    passIndex pidx(cGlob.regionSide, hSweptConst.rBase);
+
+    int *dPyramid, *dBridge;
+    int passAlloc = 4 * sizeof(int) * pidx.nPass;
+    cudaMalloc((void **) &dPyramid, passAlloc);
+    cudaMalloc((void **) &dBridge, passAlloc);
+    cudaMemcpy(pidx.&pyramid, dPyramid, passAlloc, cudaMemcpyHostToDevice);
+    cudaMemcpy(pidx.&bridge, dBridge, passAlloc, cudaMemcpyHostToDevice);
+
     if (gpuRegions) 
     {
-        cudaFuncAttributes attr;
-        memset(&attr, 0, sizeof(attr));
-        cudaFuncGetAttributes(&attr, func);
-        const size_t smem = attr.maxDynamicSharedSizeBytes; //This needs byte size.
-        hSweptConst.nSmem = smem/sizeof(states);
-
-
         cudaMemcpyToSymbol(dSweptConst, &hSweptConst, sizeof(sweptConst));
-        
         cudaMalloc((void **) &regionSelector, sizeof(states *) * gpuRegions);
         for (int i=0; i<gpuRegions; i++)
         {
             setgpuRegion <<< 1, 1 >>> (regionSelector, regionals[i]->dState, i);
         }
     }
+    stepNow = regionals[0]->tStep;
 
-    if (gpuRegions)     wholeOctahedron <<< gpuRegions, tdim, smem >>>(regionSelector, stepNow);
-    else                wholeOctahedronCPU(regionals[0]);
-    
+    // UP_PYRAMID
+    if (gpuRegions)     wholeOctahedron <<< gpuRegions, tdim, smem >>>(regionSelector, stepNow, false, 1);
+    else                wholeOctahedronCPU(regionals[0]->state, stepNow, 1);
+
+    // PASSING SW
+ 
+    // Then BRIDGES NE - .
+    if (gpuRegions)     homeBridge <<< gpuRegions, tdim, smem >>> (regionSelector, stepNow);
+    else                homeBridgeCPU (regionals[0]->state, stepNow);
+
+    // PASSING INTERIOR EDGES - .
+
     while (regionals[0]->tStamp < cGlob.tf)
     {   
-        stepNow = regionals[0]->tStep;
-        if (gpuRegions)    wholeOctahedron <<< gpuRegions, tdim >>>(regionSelector, stepNow);
-        else               wholeOctahedronCPU(regionals[0]);
+        // OFFSET WHOLE OCTAHEDRON
+        if (gpuRegions)     wholeOctahedron <<< gpuRegions, tdim, smem >>>(regionSelector, stepNow, false, 2);
+        else                wholeOctahedronCPU(regionals[0]->state + hSweptConst.splitOffset, stepNow, 2);
+
+        // PASSING NE
 
         for (auto r: regionals)
         {
             for (auto n: r->neighbors)
             {
-            
                 if (n->sameProc) //Only occurs in gpu blocks.
                 {
                     n->printer();
@@ -249,7 +417,6 @@ void sweptWrapper(std::vector <Region *> &regionals)
                     }
                     else
                     {
-        
                         r->bufMessage(0, n->sidx);
                         cpuBufCopy(r->stateRows, r->recvBuffer, n->sidx, 0);
                     } // End gpu vs cpu receiver choice. 
@@ -259,14 +426,3 @@ void sweptWrapper(std::vector <Region *> &regionals)
     }                 // End while loop over all timesteps
     cudaFree(regionSelector);
 }
-
-
-// void applyBC(states *state, int ty, int pt)
-// {
-//     // Like if-dirichilet
-//     // Works for whole
-//     state[ty*pt] = sBound[ty];
-//     // If reflective
-//     // state[ty*pt] = state[pt-2] or state[pt+2]
-// }
-

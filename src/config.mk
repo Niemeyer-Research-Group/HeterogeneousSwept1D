d4efbb9c1c2789ec0375ae03a6c3583115b61592
osumageed <mageed@oregonstate.edu>
2018-05-15 10:21:37 -0700

    Fixed Makefile on cluster. Still getting unaccountable fault in old heat

--- src/config.mk
+++ src/config.mk
@@ -1,23 +1,21 @@
 #ESSENTIAL PATHS.  Sure most are probably on your system, but let's make it official
 
-#ESSENTIAL PATHS.  Sure most are probably on your system, but let's make it official
-
-PREFIX          =$(HOME)/Documents/1_SweptRuleResearch/hSweep
-NVCC            =$(shell which nvcc)
-MPICXX          =$(shell which mpicxx)
+PREFIX          =$(MSCRATCH)/HeterogeneousSwept1D
+NVCC            =$(CUDAPATH)/bin/nvcc
+MPICXX          =$(MPIPATH)/bin/mpicxx)
 SOURCEPATH      =$(PREFIX)/src
-CC_ICUDAPATH    =/usr/local/cuda/include
-CC_IMPIPATH     =/usr/local/openmpi/include
+CC_ICUDAPATH    =$(CUDAPATH)/include
+CC_IMPIPATH     =$(MPIPATH)/include
 CC_LMPIPATH     =
 CC_LCUDAPATH    =
 CUDAFLAGS       =-gencode arch=compute_35,code=sm_35 -restrict  --ptxas-options=-v
 CFLAGS          =-O3 --std=c++11 -w
-LIBMPI          =-lmpi 
+LIBMPI          =-lmpich -lopa -lmpl -lrt -lpthread 
 LIBCUDA         =-lcudart -lcuda
 
 # Additional PATHS
 IPATH           =
-LPATH           =
+LPATH           =$(MPIPATH)/lib $(INTELLIB) $(CUDAPATH)/lib64
 
 # Additional Libs
 LIBS            =-lm 
